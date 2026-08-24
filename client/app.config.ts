e9afe528243ada88c292464dfefcf5c8c004472c
user454379452 <1923488948949338-user454379452@noreply.coze.cn>
2026-08-24 15:09:02 +0800

    chore: 提交 1.0.12 (版本 1.0.1) 到 TestFlight，准备新建 ASC 版本提交审核
    Coze-Commit-Type: user
    Coze-User-ID: 1923488948949338
    Coze-Conversation-ID: 11825402

--- client/app.config.ts
+++ client/app.config.ts
@@ -11,7 +11,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     "name": appName,
     "slug": slugAppName,
-    "version": "1.0.0",
+    "version": "1.0.1",
     "orientation": "portrait",
     "icon": "./assets/images/icon.png",
     "scheme": "myapp",
